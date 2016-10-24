96f6d8b3532f9d2d44353dfbccc2c9b10047c776
Andy Russell <andrew.russell@wallawalla.edu>
2016-10-23 22:50:19 -0700

    Implemented the storage engine.

--- src/storage.cpp
+++ src/storage.cpp
@@ -2,28 +2,231 @@
 // Created by Andrew Russell on 10/19/16.
 //
 
+#include <iostream>
 #include "include/storage.h"
 
-Storage::Storage() {
+Storage::Storage(SafeString *key) : Storage(key, NULL) {}
 
+Storage::Storage(SafeString *key, SafeString *safe) {
+    if (key != NULL) {
+        // Extract key into array
+        char *cp_key = (char *) malloc(key->get_max_length());
+        memset(cp_key, 0, key->get_max_length());
+        unsigned key_len;
+        key->get_data((unsigned char *) cp_key, &key_len);
+
+        // Key size checking
+        if (key_len < sizeof(this->key))
+            throw new runtime_error("Provided key was too short");
+
+        // Copy the first 16 bytes into the key
+        memcpy(this->key, cp_key, sizeof(this->key));
+    } else {
+        throw new runtime_error("Key must be provided");
+    }
+// TODO: Move this stuff into its own function and call it from `load` and `save`.
+//        // Decrypt the password safe ///////////////////////////////////////////
+//
+//        char *cp_safe = (char *) malloc(safe->get_max_length());
+//        memset(cp_safe, 0, safe->get_max_length());
+//        unsigned safe_len;
+//        safe->get_data((unsigned char *) cp_safe, &safe_len);
+//
+//        // Assemble a decryption object
+//        EVP_CIPHER_CTX *ctx;
+//        if (!(ctx = EVP_CIPHER_CTX_new()))
+//            throw new runtime_error(
+//                    "Abstract cipher object failed to initialize");
+//        if (EVP_DecryptInit(ctx, EVP_aes_128_cbc(), this->key, this->key) != 1)
+//            throw new runtime_error(
+//                    "AES decryption object failed to initialize");
+//
+//        // Build a temporary container for the plain text
+//        unsigned char *plain = (unsigned char *) malloc(safe_len);
+//        if (plain == NULL)
+//            throw new runtime_error(
+//                    "Unable to allocate extra memory for plain text");
+//        memset(plain, 0, safe_len);
+//
+//        // Decrypt the cipher text
+//        int plain_len_update, plain_len_final = 0;
+//        if (EVP_DecryptUpdate(ctx, plain, &plain_len_update,
+//                              (const unsigned char *) cp_safe,
+//                              (int) safe_len) != 1)
+//            throw new runtime_error("Encryption update failed");
+//        if (EVP_DecryptFinal_ex(ctx, plain + plain_len_update,
+//                                &plain_len_final) !=
+//            1)
+//            throw new runtime_error("Encryption finalization failed");
+//        int plain_len = plain_len_update + plain_len_final;
+//
+//        // Build decrypted string objects
+//        string s_plain((char *) plain, (unsigned) plain_len);
+//
+//        // Clean up decryption mechanics
+//        memset(plain, 0, safe_len);
+//        free((void *) plain);
+//        EVP_CIPHER_CTX_free(ctx);
 }
 
 Storage::~Storage() {
 
 }
 
-bool Storage::get(unsigned int id, entry *e) {
+void Storage::load(SafeString *ss) {
+    string s = ss->get_data();
+
+    // TODO: put decryption here
+
+    // Ensure the length isn't entirely invalid
+    if (s.length() < SHA512_DIGEST_LENGTH)
+        throw new runtime_error("Safe file is too short");
+
+    // Extract relevant sections
+    string safe(s.substr(0, s.length() - SHA512_DIGEST_LENGTH));
+    string digest(s.substr(s.length() - SHA512_DIGEST_LENGTH));
+
+    // Fetch and compute digests
+    unsigned char given_digest[SHA512_DIGEST_LENGTH],
+            computed_digest[SHA512_DIGEST_LENGTH];
+    memcpy(&given_digest,
+           digest.data(),
+           digest.length());
+    SHA512((const unsigned char *) safe.data(),
+           safe.length(),
+           computed_digest);
+
+    // Check if computed and given digests match
+    if (memcmp(given_digest, computed_digest, SHA512_DIGEST_LENGTH) != 0) {
+        throw new runtime_error("File verification failed");
+    }
+
+    while (safe.length()) {
+        // Get serialized entry length
+        unsigned len;
+        memcpy(&len, safe.data(), sizeof(len));
+
+        // Build substring containing just this entry's data
+        string buf((char *) safe.data() + sizeof(len), len);
+
+        // Perform the deserialization
+        entry e;
+        e.deserialize(buf);
+
+        // Store the results
+        this->entries.push_back(e);
+
+        // Step to the next entry
+        s.erase(0, sizeof(len) + len);
+    }
+}
+
+SafeString *Storage::save() {
+    ostringstream oss(ios::app);
+
+    // Serialize all entries and save to the output stream
+    for (unsigned i = 0; i < this->entries.size(); i++) {
+        // Perform the actual serialization
+        string s = this->entries[i].serialize();
+
+        // Buffer the length
+        unsigned len = (unsigned) s.length();
+        char ca_len[4];
+        memcpy(ca_len, &len, sizeof(len));
+
+        // Dump to output stream
+        oss << ca_len;
+        oss << s;
+    }
+
+    // Compute and append SHA512 digest
+    string s = oss.str();
+    unsigned char digest[SHA512_DIGEST_LENGTH];
+    SHA512((const unsigned char *) s.data(), s.length(), digest);
+
+    oss << string((char *) digest, SHA512_DIGEST_LENGTH);
+    s = oss.str();
+
+    // TODO: put encryption here
+
+    // Bundle and return the entries
+    return new SafeString(s);
+}
+
+vector<entry> Storage::list() {
+    return vector<entry>(this->entries);
+}
+
+bool Storage::create(entry *e) {
+    // Try to assign a random, unique ID
+    unsigned tries = 1000;  // (9x10^-300)% chance of not finding a unique value in a set of 2^31 entries, which is prohibitively large
+    while (true) {
+        // Assign a totally random ID
+        e->id = (unsigned) rand();
+
+        // Check for collisions
+        bool found_match = false;
+        for (unsigned i = 0; i < this->entries.size(); i++) {
+            if (this->entries[i].id == e->id) {
+                found_match = true;
+                break;
+            }
+        }
+
+        // If a match wasn't found, the ID is unique
+        if (!found_match) {
+            break;
+        }
+
+        // Note how many times we tried and failed
+        if (--tries == 0) {
+            // Failed too many times
+            return false;
+        }
+    }
+
+    // Store the entry in our table
+    this->entries.push_back(*e);
+
+    // Creation succeeded!
+    return true;
+}
+
+bool Storage::get(entry *e) {
+    for (unsigned i = 0; i < this->entries.size(); i++) {
+        if (e->id == this->entries[i].id) {
+            memcpy(e, &this->entries[i], sizeof(entry));
+            return true;
+        }
+    }
+
     return false;
 }
 
-bool Storage::set(unsigned int id, entry *e) {
+bool Storage::set(entry *e) {
+    for (unsigned i = 0; i < this->entries.size(); i++) {
+        if (e->id == this->entries[i].id) {
+            memcpy(&this->entries[i], e, sizeof(entry));
+            return true;
+        }
+    }
+
     return false;
 }
 
-void Storage::apply_key(SafeString key) {
+bool Storage::remove(entry *e) {
+    for (unsigned i = 0; i < this->entries.size(); i++) {
+        if (e->id == this->entries[i].id) {
+            entry e_i = this->entries[i];
+            delete e_i.title;
+            delete e_i.username;
+            delete e_i.password;
 
+            this->entries.erase(this->entries.begin() + i);
+            return true;
+        }
+    }
+
+    return false;
 }
 
-void Storage::apply_password(SafeString password) {
-
-}
--- test/test_storage.cpp
+++ test/test_storage.cpp
@@ -11,7 +11,88 @@
 
 using namespace std;
 
-TEST_CASE("Description goes here!") {
-    Storage *s = new Storage();
+TEST_CASE("Check initialization") {
+    SafeString key(
+            (unsigned char *) "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f",
+            16);
+    Storage *s = new Storage(&key);
     REQUIRE(s != NULL);
+}
+
+TEST_CASE("Try save and load operations on an empty safe") {
+    SafeString key(
+            (unsigned char *) "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f",
+            16);
+    Storage *s = new Storage(&key);
+    SafeString *ss = s->save();
+
+    Storage *s2 = new Storage(&key);
+    s2->load(ss);
+
+    free(ss);
+}
+
+TEST_CASE("Test create, get, set, delete, and list") {
+    vector<entry> entries;
+
+    SafeString key(
+            (unsigned char *) "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f",
+            16);
+    Storage *s = new Storage(&key);
+
+    entries = s->list();
+    REQUIRE(entries.size() == 0);
+
+    entry e;
+    e.id = 0;
+    e.title = new SafeString("EntryTitle");
+    e.username = new SafeString("EntryUsername");
+    e.password = new SafeString("EntryPassword");
+    s->create(&e);
+    REQUIRE(e.id != 0); // Will fail 1 in 2^32 tests
+
+    entries = s->list();
+    REQUIRE(entries.size() == 1);
+
+    entry e2;
+    e2.id = e.id;
+    s->get(&e2);
+    REQUIRE(e.title->get_data() == e2.title->get_data());
+    REQUIRE(e.username->get_data() == e2.username->get_data());
+    REQUIRE(e.password->get_data() == e2.password->get_data());
+
+    entries = s->list();
+    REQUIRE(entries.size() == 1);
+
+    free(e2.title);
+    e2.title = new SafeString("UpdatedEntryTitle");
+    s->set(&e2);
+    s->get(&e2);
+    REQUIRE(e.id == e2.id);
+    REQUIRE(e2.title->get_data().compare("UpdatedEntryTitle") == 0);
+
+    entries = s->list();
+    REQUIRE(entries.size() == 1);
+
+    s->remove(&e2);
+
+    entries = s->list();
+    REQUIRE(entries.size() == 0);
+}
+
+TEST_CASE("Check entry serialization and deserialization") {
+    entry e = {};
+    e.id = 63;
+    e.title = new SafeString("Title");
+    e.username = new SafeString("Username");
+    e.password = new SafeString("Password");
+    string serialized = e.serialize();
+
+    entry e2 = {};
+    e2.deserialize(serialized);
+
+    REQUIRE(e.id == e2.id);
+    REQUIRE(e.title->get_data() == e2.title->get_data());
+    REQUIRE(e.username->get_data() == e2.username->get_data());
+    REQUIRE(e.password->get_data() == e2.password->get_data());
 }
