e5ea3fe1f5f75cda50e2a1c3f7cdeb9a17298d9c
Andy Russell <andrew.russell@wallawalla.edu>
2016-10-27 15:12:16 -0700

    Included TCLAP

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 
+#include <tclap/CmdLine.h>
+
 using namespace std;
 
 int main() {
+    TCLAP::CmdLine cmd("Command description message", ' ', "0.9");
+    (void) cmd;
+
     cout << "Hello CPP-Pass" << endl;
+
     return 0;
 }
